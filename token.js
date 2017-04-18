5e2bd11cb6ebb93592c6ef245ac99f04b289ee2c
codinghobby@gmail.com <codinghobby@gmail.com>
2017-04-18 21:32:28 +0200

    Testing responses

--- token.js
+++
@@ -1,1 +0,0 @@
-module.exports = 'access_granted'
---
+++ tokens.js
@@ -0,0 +1,4 @@
+module.exports = {
+	pwd: 'access_granted',
+	token: 'EAAZAf9MOKYjMBAOsuEbtfZCZBNgqg9JPcdLC5ZCyuZAFGKaPf7UL1FytgYbImPbeu8DwOZAe0H9XcBgRgj98EmnZBw6s2BwlsTOKaTwYKoZB8nZAGwYIZAUVL1gyN743tZCe88HZALBu3CnthEqxNYYqCjsw8D4NxtC2O42m1jOM4L5sRAZDZD' 
+}
