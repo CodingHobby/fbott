bdc4e3595f0e3f7ee44e9e42b73ab0f7053178dd
codinghobby@gmail.com <codinghobby@gmail.com>
2017-04-18 21:24:04 +0200

    First test

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express'),
 	app = express(),
 	port = process.env.PORT || 3000,
 	token = require('./token'),
+	bodyParser = require('body-parser'),
 	server = app.listen(port, function () {
 		console.log('App running on port ' + port)
 	})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 
 app.get('/webhook', function (req, res) {
 	if (req.query['hub.mode'] === 'subscribe' &&
@@ -16,3 +22,38 @@ app.get('/webhook', function (req, res) {
 		res.sendStatus(403);
 	}
 })
+
+app.post('/webhook', function (req, res) {
+	var data = req.body;
+
+	// Make sure this is a page subscription
+	if (data.object === 'page') {
+
+		// Iterate over each entry - there may be multiple if batched
+		data.entry.forEach(function (entry) {
+			var pageID = entry.id;
+			var timeOfEvent = entry.time;
+
+			// Iterate over each messaging event
+			entry.messaging.forEach(function (event) {
+				if (event.message) {
+					receivedMessage(event);
+				} else {
+					console.log("Webhook received unknown event: ", event);
+				}
+			});
+		});
+
+		// Assume all went well.
+		//
+		// You must send back a 200, within 20 seconds, to let us know
+		// you've successfully received the callback. Otherwise, the request
+		// will time out and we will keep trying to resend.
+		res.sendStatus(200);
+	}
+});
+
+function receivedMessage(event) {
+	// Putting a stub for now, we'll expand it in the following steps
+	console.log("Message data: ", event.message);
+}
